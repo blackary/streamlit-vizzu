e424cfa027146b50f28eb81b6a1af1d22dbcaf1c
Zachary Blackwood <zachary@streamlit.io>
2022-11-29 17:01:16 -0500

    SUPED WWWWWIP

--- src/streamlit_vizzu/__init__.py
+++ src/streamlit_vizzu/__init__.py
@@ -1,36 +1,287 @@
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional
 
 import streamlit as st
 import streamlit.components.v1 as components
+from bs4 import BeautifulSoup
+from ipyvizzu.chart import Animate, Animation, Chart, DisplayTemplate
 
 # Tell streamlit that there is a component called streamlit_vizzu,
 # and that the code to display that component is in the "frontend" folder
 frontend_dir = (Path(__file__).parent / "frontend").absolute()
 _component_func = components.declare_component(
-	"streamlit_vizzu", path=str(frontend_dir)
+    "streamlit_vizzu", path=str(frontend_dir)
 )
+
 
 # Create the python function that will be called
 def streamlit_vizzu(
+    chart: Any,
     key: Optional[str] = None,
+    step: int = 0,
 ):
     """
     Add a descriptive docstring
     """
+    # components.html(chart._repr_html_(), height=600)
+    # st.write(chart._repr_html_())
+    html = chart._repr_html_()
+    soup = BeautifulSoup(html, "html.parser")
+    # div_id = soup.find("div").get("id")
+    div_id = f"{key}_vizzu"
+    raw_script = soup.find("script").get_text()
+    # st.code(raw_script)
+
+    script = """
+    window.ipyvizzu.createChart(document.getElementById("55861cd"), '4aa31fc', 'https://cdn.jsdelivr.net/npm/vizzu@~0.6.0/dist/vizzu.min.js', '100%', '360px');
+    window.ipyvizzu.animate(document.getElementById("55861cd"), '4aa31fc', 'manual', false, lib => { return {"data": {"series": [{"name": "Region", "type": "dimension", "values": ["North", "North", "North", "North", "South", "South", "South", "South", "East", "East", "East", "East", "West", "West", "West", "West"]}, {"name": "Product", "type": "dimension", "values": ["Shoes", "Handbags", "Gloves", "Accessories", "Shoes", "Handbags", "Gloves", "Accessories", "Shoes", "Handbags", "Gloves", "Accessories", "Shoes", "Handbags", "Gloves", "Accessories"]}, {"name": "Sales", "type": "measure", "values": [4500.0, 7400.0, 2350.0, 8200.0, 3850.0, 6200.0, 3100.0, 10500.0, 2300.0, 3800.0, 7250.0, 5600.0, 4100.0, 6800.0, 3650.0, 7600.0]}, {"name": "Revenue [$]", "type": "measure", "values": [202500.0, 296000.0, 70500.0, 147600.0, 173250.0, 248000.0, 93000.0, 189000.0, 103500.0, 152000.0, 217500.0, 100800.0, 184500.0, 272000.0, 109500.0, 136800.0]}]}} }, undefined);
+    window.ipyvizzu.animate(document.getElementById("55861cd"), '4aa31fc', 'manual', false, lib => { return {"data": {"filter": record => { return (record['Product'] == 'Shoes') }}, "config": {"x": "Region", "y": ["Sales", "Product"], "label": "Sales", "color": "Product", "title": "Sales of Shoes"}} }, undefined);
+    window.ipyvizzu.animate(document.getElementById("55861cd"), '4aa31fc', 'manual', false, lib => { return {"data": {"filter": record => { return (record['Product'] == 'Shoes' || record['Product'] == 'Handbags') }}, "config": {"title": "Sales of Shoes & Handbags"}} }, {"delay": 0.1});
+    """.replace(
+        "55861cd", div_id
+    )
+    st.expander("Show sent code").code(script)
+
+    st.expander("Show code").code(html, language="html")
     component_value = _component_func(
+        div_id=div_id,
+        script=script,
+        step=step,
         key=key,
     )
 
     return component_value
 
 
-def main():
-    st.write("## Example")
-    value = streamlit_vizzu()
+class VizzuChart:
+    def __init__(self, chart, key: Optional[str] = None):
+        self.chart = chart
+        self.key = key
+        self.html = self.chart._repr_html_()
+        self.animations: list[str] = []
+        self.div_id = f"{self.key}_vizzu"
+        self.chart_id = f"{self.key}_vizzu_chart"
+        # self.show()
 
-    st.write(value)
+    def _repr_html_(self):
+        return self.chart._repr_html_()
+
+    # def show(self, script: Optional[str] = None):
+    def show(self):
+        # streamlit_vizzu(self)
+        # div_id = self.chart._display_target.value
+        soup = BeautifulSoup(self.html, "html.parser")
+        raw_div_id = soup.find("div").get("id")
+        raw_chart_id = self.chart._chart_id
+        script = soup.find("script").get_text()
+        script += "\n".join(self.animations)
+
+        script = script.replace(raw_div_id, self.div_id)
+        script = script.replace(raw_chart_id, self.chart_id)
+
+        # st.expander("Show code").code(html, language="html")
+        component_value = _component_func(
+            div_id=self.div_id,
+            script=script,
+            key=self.key,
+        )
+
+        return component_value
+
+    def animation_to_js(self, *animations: Animation, **options: Any):
+        animation = self.chart._merge_animations(animations)
+        animate = Animate(animation, options)
+        return DisplayTemplate.ANIMATE.format(
+            display_target=self.chart._display_target.value,
+            chart_id=self.chart._chart_id,
+            scroll=str(self.chart._scroll_into_view).lower(),
+            **animate.dump(),
+        )
+
+    def animate(self, *animations, **options):
+        js = self.animation_to_js(*animations, **options)
+
+        self.animations = [js]
+
+        # self.chart.animate(*args, **kwargs)
+        # return self.show()
+        # self.show()
+
+
+def main():
+    import pandas as pd
+    from st_vizzu import Config, Data, bar_chart
+
+    df = pd.DataFrame(
+        {
+            "a": [4, 5, 6, 7, 8],
+            "b": [10, 20, 30, 40, 50],
+            "c": [100, 50, -30, -50, -70],
+        }
+    )
+
+    chart = bar_chart(df, ["a"], ["b"], "A vs B")
+
+    st.write("## Example")
+    # chart._repr_html_()
+    # value = streamlit_vizzu(chart)
+    vchart = VizzuChart(chart, key="vizzu")
+
+    vchart.show()
+
+    st.write("## Animate")
+
+    vchart.animate(Data.filter(None), Config({"title": "NO FILTER"}), delay=2)
+
+    # st.write(value)
+
+
+def main2():
+    import pandas as pd
+    from st_vizzu import Chart, Config, Data, bar_chart
+
+    data_frame = pd.read_csv(
+        "https://raw.githubusercontent.com/vizzuhq/ipyvizzu/main/docs/examples/stories/sales/sales.csv",
+        dtype={"tenure": str},
+    )
+    data = Data()
+    data.add_data_frame(data_frame)
+
+    # chart = Chart(width="100%", height="360px", display="manual")
+    chart = Chart(width="100%", height="360px")
+
+    chart.animate(data)
+
+    chart.animate(
+        Data.filter("record['Product'] == 'Shoes'"),
+        Config(
+            {
+                "x": "Region",
+                "y": ["Sales", "Product"],
+                "label": "Sales",
+                "color": "Product",
+                "title": "Sales of Shoes",
+            }
+        ),
+    )
+
+    chart.animate(
+        Data.filter("record['Product'] == 'Shoes' || record['Product'] == 'Handbags'"),
+        Config({"title": "Sales of Shoes & Handbags"}),
+        delay=0.1,
+    )
+
+    chart.animate(
+        Data.filter("record['Product'] != 'Accessories'"),
+        Config({"title": "Sales of Shoes, Handbags & Gloves"}),
+        delay=0.1,
+    )
+
+    chart.animate(
+        Data.filter(None), Config({"title": "Sales of All Products"}), delay=2
+    )
+
+    chart.animate(
+        Config(
+            {
+                "y": ["Revenue [$]", "Product"],
+                "label": "Revenue [$]",
+                "title": "Revenue of All Products",
+            }
+        ),
+        delay=2,
+    )
+
+    chart.animate(Config({"x": ["Region", "Revenue [$]"], "y": "Product"}), delay=3)
+
+    chart.animate(Config({"x": "Revenue [$]", "y": "Product"}))
+
+    chart.animate(Config({"coordSystem": "polar", "sort": "byValue"}), delay=1)
+
+    # chart.show()
+
+    step = st.slider("Step", 0, 9, 0)
+    streamlit_vizzu(chart, key="a_key", step=step)
+
+
+def main3():
+    import pandas as pd
+    from st_vizzu import Chart, Config, Data, bar_chart
+
+    data_frame = pd.read_csv(
+        "https://raw.githubusercontent.com/vizzuhq/ipyvizzu/main/docs/examples/stories/sales/sales.csv",
+        dtype={"tenure": str},
+    )
+    data = Data()
+    data.add_data_frame(data_frame)
+
+    # chart = Chart(width="100%", height="360px", display="manual")
+    chart = Chart(width="100%", height="360px", display="manual")
+
+    chart.animate(data)
+
+    vchart = VizzuChart(chart, key="vizzu")
+
+    if st.button("Step 1"):
+        vchart.animate(
+            Data.filter("record['Product'] == 'Shoes'"),
+            Config(
+                {
+                    "x": "Region",
+                    "y": ["Sales", "Product"],
+                    "label": "Sales",
+                    "color": "Product",
+                    "title": "Sales of Shoes",
+                }
+            ),
+        )
+
+    if st.button("Step 2"):
+        vchart.animate(
+            Data.filter(
+                "record['Product'] == 'Shoes' || record['Product'] == 'Handbags'"
+            ),
+            Config({"title": "Sales of Shoes & Handbags"}),
+            delay=0.1,
+        )
+
+    if st.button("Add handbags"):
+        vchart.animate(
+            Data.filter("record['Product'] != 'Accessories'"),
+            Config({"title": "Sales of Shoes, Handbags & Gloves"}),
+            delay=0.1,
+        )
+
+    if st.button("All products"):
+        vchart.animate(
+            Data.filter(None), Config({"title": "Sales of All Products"}), delay=2
+        )
+
+    if st.button("Revenue"):
+        vchart.animate(
+            Config(
+                {
+                    "y": ["Revenue [$]", "Product"],
+                    "label": "Revenue [$]",
+                    "title": "Revenue of All Products",
+                }
+            ),
+            delay=2,
+        )
+
+    if st.button("Region"):
+        vchart.animate(
+            Config({"x": ["Region", "Revenue [$]"], "y": "Product"}), delay=3
+        )
+
+    if st.button("Revenue by product"):
+        vchart.animate(Config({"x": "Revenue [$]", "y": "Product"}))
+
+    if st.button("Polar"):
+        vchart.animate(Config({"coordSystem": "polar", "sort": "byValue"}), delay=1)
+
+    vchart.show()
 
 
 if __name__ == "__main__":
-    main()
+    main3()
--- src/streamlit_vizzu/frontend/main.js
+++ src/streamlit_vizzu/frontend/main.js
@@ -7,6 +7,132 @@ function sendValue(value) {
   Streamlit.setComponentValue(value)
 }
 
+class IpyVizzu
+{
+    constructor()
+    {
+        IpyVizzu.inhibitScroll = false;
+        IpyVizzu.nbconvert = false;
+        document.addEventListener('wheel', (evt) => { IpyVizzu.inhibitScroll = true }, true);
+        document.addEventListener('keydown', (evt) => { IpyVizzu.inhibitScroll = true }, true);
+        document.addEventListener('touchstart', (evt) => { IpyVizzu.inhibitScroll = true }, true);
+
+        this.elements = {};
+        this.charts = {};
+
+        this.snapshots = {};
+        this.displays = {};
+
+        this.events = {};
+        this.loaded = {};
+        this.libs = {};
+    }
+
+    static clearInhibitScroll(element)
+    {
+        if (IpyVizzu.nbconvert) IpyVizzu._hide(element);
+        IpyVizzu.inhibitScroll = false;
+    }
+
+    createChart(element, chartId, vizzulib, divWidth, divHeight) {
+        this.elements[chartId] = document.createElement("div");
+        this.elements[chartId].style.cssText = `width: ${divWidth}; height: ${divHeight};`;
+        this.loaded[chartId] = import(vizzulib);
+        this.charts[chartId] = this.loaded[chartId].then(Vizzu => {
+            this.libs[chartId] = Vizzu.default;
+            return new Vizzu.default(this.elements[chartId]).initializing
+        });
+        this._moveHere(chartId, element);
+    }
+
+    animate(element, chartId, displayTarget, scrollEnabled, getChartTarget, chartAnimOpts)
+    {
+        if (IpyVizzu.nbconvert) IpyVizzu._hide(element);
+        if (displayTarget === 'end') this._moveHere(chartId, element);
+        this.charts[chartId] = this.charts[chartId].then(chart => {
+            if (displayTarget === 'actual') this._moveHere(chartId, element);
+            this._scroll(chartId, scrollEnabled);
+            let chartTarget = getChartTarget(this.libs[chartId]);
+            if (typeof chartTarget === 'string') chartTarget = this.snapshots[chartTarget];
+            return chart.animate(chartTarget, chartAnimOpts);
+        });
+    }
+
+    store(element, chartId, id)
+    {
+        if (IpyVizzu.nbconvert) IpyVizzu._hide(element);
+        this.charts[chartId] = this.charts[chartId].then(chart => {
+            this.snapshots[id] = chart.store();
+            return chart;
+        });
+    }
+
+    feature(element, chartId, name, enabled)
+    {
+        if (IpyVizzu.nbconvert) IpyVizzu._hide(element);
+        this.charts[chartId] = this.charts[chartId].then(chart => {
+            chart.feature(name, enabled);
+            return chart;
+        });
+    }
+
+    setEvent(element, chartId, id, event, handler)
+    {
+        if (IpyVizzu.nbconvert) IpyVizzu._hide(element);
+        this.charts[chartId] = this.charts[chartId].then(chart => {
+            this.events[id] = handler;
+            chart.on(event, this.events[id]);
+            return chart;
+        });
+    }
+
+    clearEvent(element, chartId, id, event)
+    {
+        if (IpyVizzu.nbconvert) IpyVizzu._hide(element);
+        this.charts[chartId] = this.charts[chartId].then(chart => {
+            chart.off(event, this.events[id]);
+            return chart;
+        });
+    }
+
+    log(element, chartId, chartProperty)
+    {
+        if (IpyVizzu.nbconvert) IpyVizzu._hide(element);
+        this.charts[chartId] = this.charts[chartId].then(chart => {
+            console.log(chart[chartProperty])
+            return chart;
+        });
+    }
+
+    _moveHere(chartId, element)
+    {
+        if (IpyVizzu.nbconvert) IpyVizzu._display(this.elements[chartId], element);
+        element.append(this.elements[chartId]);
+    }
+
+    _scroll(chartId, enabled)
+    {
+        if (!IpyVizzu.inhibitScroll && enabled) {
+            this.elements[chartId].scrollIntoView({ behavior: "auto", block: "center" });
+        }
+    }
+
+    static _hide(element) {
+        document.getElementById(element.selector.substring(1)).parentNode.style.display = 'none';
+    }
+
+    static _display(prevElement, element) {
+        if (prevElement.parentNode) {
+            prevElement.parentNode.style.display = "none";
+        }
+        document.getElementById(element.selector.substring(1)).parentNode.style.display = 'flex';
+        document.getElementById(element.selector.substring(1)).parentNode.style.margin = 'auto';
+    }
+}
+
+window.IpyVizzu = IpyVizzu;
+window.ipyvizzu = new window.IpyVizzu();
+
 /**
  * The component's render function. This will be called immediately after
  * the component is initially loaded, and then again every time the
@@ -14,14 +140,60 @@ function sendValue(value) {
  */
 function onRender(event) {
   // Only run the render code the first time the component is loaded.
+  const {div_id, script} = event.detail.args
+
+  lines = script.split("\n")
+  const isCreateChartLine = (line) => line.indexOf("ipyvizzu.createChart") != -1;
+  idx = lines.findIndex(isCreateChartLine)
+  create_chart_lines = lines.slice(0, idx + 1)
+  other_lines = lines.slice(idx + 1)
+  //create_chart = lines.filter(line => line.indexOf("window.ipyvizzu.createChart") != -1)[0]
+  //other_lines = lines.filter(line => line.indexOf("window.ipyvizzu.createChart") == -1)
+
+  //script_lines = script.split("\n")
+
   if (!window.rendered) {
     // You most likely want to get the data passed in like this
-    // const {input1, input2, input3} = event.detail.args
+
+    //console.log(html)
+
+    root = document.getElementById("root")
+
+    element = document.createElement("div")
+    element.id = div_id
+
+    root.append(element)
+
+    //eval(script_lines[0])
+    //eval(script)
+    //create_chart = script.split("\n").filter(line => line.indexOf("window.ipyvizzu.createChart") != -1)[0]
+    eval(create_chart_lines.join("\n"))
+
+    //root.innerHTML = "<h1>Yo!</h1>"
+
+    //root.innerHTML = "<div id='500910c'></div>"
+
+    //root.innerHTML = html
+
+    /*
+    window.ipyvizzu.createChart(document.getElementById("500910c"), 'f5e861a', 'https://cdn.jsdelivr.net/npm/vizzu@~0.6.0/dist/vizzu.min.js', '700px', '600px');
+    window.ipyvizzu.animate(document.getElementById("500910c"), 'f5e861a', 'manual', false, lib => { return {"data": {"series": [{"name": "a", "type": "measure", "values": [4.0, 5.0, 6.0, 7.0, 8.0]}, {"name": "b", "type": "measure", "values": [10.0, 20.0, 30.0, 40.0, 50.0]}, {"name": "c", "type": "measure", "values": [100.0, 50.0, -30.0, -50.0, -70.0]}]}} }, undefined);
+    window.ipyvizzu.animate(document.getElementById("500910c"), 'f5e861a', 'manual', false, lib => { return {"config": lib.presets.bar({'x': ['a'], 'y': ['b'], 'title': 'A vs B'})} }, undefined);
+
+    element = document.createElement("div")
+
+    element.innerHTML = html
+
+    //console.log(html)
+    document.body.appendChild(element)
+    */
 
     // You'll most likely want to pass some data back to Python like this
     // sendValue({output1: "foo", output2: "bar"})
     window.rendered = true
   }
+
+  eval(other_lines.join("\n"))
 }
 
 // Render the component whenever python send a "render event"
@@ -29,4 +201,4 @@ Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender)
 // Tell Streamlit that the component is ready to receive events
 Streamlit.setComponentReady()
 // Render with the correct height, if this is a fixed-height component
-Streamlit.setFrameHeight(100)
+Streamlit.setFrameHeight(500)
