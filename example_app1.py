62900b7a964a12c4798828f013c9e5c0f7f0e3ca
Zachary Blackwood <zachary@streamlit.io>
2022-11-30 10:23:56 -0500

    Get example app working

---
+++ example_app1.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import streamlit as st
+from ipyvizzu.animation import Config, Data
+from ipyvizzu.chart import Chart
+
+from streamlit_vizzu import VizzuChart
+
+data_frame = pd.read_csv(
+    "https://raw.githubusercontent.com/vizzuhq/ipyvizzu/main/docs/examples/stories/sales/sales.csv",
+    dtype={"tenure": str},
+)
+data = Data()
+data.add_data_frame(data_frame)
+
+chart = Chart(width="100%", height="360px", display="manual")
+
+chart.animate(data)
+
+vchart = VizzuChart(chart, key="vizzu")
+
+items: list[str] = st.multiselect(
+    "Products",
+    ["Shoes", "Handbags", "Gloves", "Accessories"],
+    ["Shoes", "Handbags", "Gloves", "Accessories"],
+)
+
+col1, col2, col3 = st.columns(3)
+
+measure: str = col1.radio("Measure", ["Sales", "Revenue [$]"])  # type: ignore
+compare_by = col2.radio("Campare by", ["Region", "Product"])
+coords = col3.radio("Coordinate system", ["Cartesian", "Polar"])
+
+filter = " || ".join([f"record['Product'] == '{item}'" for item in items])
+title = f"{measure} of " + ", ".join(items)
+
+if compare_by == "Product":
+    x = ["Region", measure]
+    y = ["Product"]
+    label = measure
+    color = "Product"
+else:
+    x = ["Product"]
+    y = [measure, "Region"]
+    label = measure
+    color = "Region"
+
+
+config = {
+    "title": title,
+    "y": y,
+    "label": measure,
+    "x": x,
+    "color": color,
+}
+
+if coords == "Polar":
+    config["coordSystem"] = "polar"
+    config["sort"] = "byValue"
+else:
+    config["coordSystem"] = "cartesian"
+
+vchart.animate(Data.filter(filter), Config(config), delay=0.1)
+vchart.show()
