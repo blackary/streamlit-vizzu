fcb9c276519d491e72000261015909553c59127e
Zachary Blackwood <zachary@streamlit.io>
2022-11-30 16:42:29 -0500

    Add POC slides for trump page

--- pages/trump.py
+++ pages/trump.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
 from ipyvizzu import Chart, Config, Data, Style
-from ipyvizzustory import Slide, Step, Story
 
 from streamlit_vizzu import VizzuChart
 
@@ -21,10 +20,9 @@ chart = Chart(width="100%", height="360px", display="manual")
 
 chart.animate(data)
 
-vchart = VizzuChart(chart, key="vizzu")
-
-vchart.animate(
-    Data.filter("record.Firsttweet === 'Igen' && record.Dummy === 'Nem'"),
+_ = """
+chart.animate(
+    Data.filter(None),
     Config(
         {
             "channels": {
@@ -34,9 +32,134 @@ vchart.animate(
                 "x": {"set": ["Period", "year", "month"]},
                 "color": "Period",
             },
-            "title": "Trump started tweeting in May '09",
         }
     ),
+    duration=0,
 )
+"""
+
+vchart = VizzuChart(chart, key="vizzu", return_clicks=False)
+
+if "slide_num" not in st.session_state:
+    try:
+        st.session_state["slide_num"] = int(
+            st.experimental_get_query_params()["slide_num"][0]
+        )
+    except (KeyError, ValueError, IndexError):
+        st.session_state["slide_num"] = 0
+
+CHANNELS = {
+    "channels": {
+        "y": {
+            "set": ["tweets"],
+        },
+        "x": {"set": ["Period", "year", "month"]},
+        "color": "Period",
+    }
+}
+
+
+previous_slide = st.button(
+    "Previous slide", disabled=st.session_state["slide_num"] == 0
+)
+if previous_slide:
+    st.session_state["slide_num"] -= 1
+
+next_slide = st.button("Next slide", disabled=st.session_state["slide_num"] >= 9)
+if next_slide:
+    st.session_state["slide_num"] += 1
+
+if st.session_state["slide_num"] == 0:
+    vchart.animate(
+        Data.filter("record.Firsttweet === 'Igen' && record.Dummy === 'Nem'"),
+        Config(
+            {
+                **CHANNELS,
+                "title": "Trump started tweeting in May '09",
+            }
+        ),
+    )
+elif st.session_state["slide_num"] == 1:
+    vchart.animate(
+        Data.filter("record.Period === 'New to Twitter' && record.Dummy === 'Nem'"),
+        Config(
+            {
+                "title": "In the first two years he wasn't very active",
+                **CHANNELS,
+            }
+        ),
+    )
+elif st.session_state["slide_num"] == 2:
+    vchart.animate(
+        Data.filter(
+            "(record.Period === 'New to Twitter' || record.Period === 'Businessman') && record.Dummy === 'Nem'"
+        ),
+        Config(
+            {
+                "title": "Then he got hooked on",
+                **CHANNELS,
+            }
+        ),
+    )
+
+elif st.session_state["slide_num"] == 3:
+    vchart.animate(
+        Data.filter(
+            "(record.Period === 'New to Twitter' || record.Period === 'Businessman' || record.Period === 'Nominee') && record.Dummy === 'Nem'"
+        ),
+        Config(
+            {
+                "title": "Interesting trend after becoming a presidential nominee",
+                **CHANNELS,
+            }
+        ),
+    )
+
+elif st.session_state["slide_num"] == 4:
+    vchart.animate(
+        Data.filter("record.Dummy === 'Nem'"),
+        Config(
+            {
+                "title": "And after he became President",
+                **CHANNELS,
+            }
+        ),
+    )
+
+elif st.session_state["slide_num"] == 5:
+    vchart.animate(
+        Config({"geometry": "area", "align": "center", **CHANNELS}),
+    )
+
+elif st.session_state["slide_num"] == 6:
+    vchart.animate(
+        Config(
+            {
+                "title": "All of Trump's tweets until May 2020",
+                **CHANNELS,
+            }
+        ),
+    )
+
+elif st.session_state["slide_num"] == 7:
+    vchart.animate(
+        Config(
+            {
+                "y": "retweetcount",
+                "title": "And the number of times these were retweeted",
+            }
+        ),
+    )
+
+elif st.session_state["slide_num"] == 8:
+    vchart.animate(
+        Config(
+            {
+                "y": "tweets",
+                "title": "Let's focus on the number of tweets for now",
+            }
+        ),
+    )
+    st.write("Sorry, only did 8 slides for now")
 
 vchart.show()
